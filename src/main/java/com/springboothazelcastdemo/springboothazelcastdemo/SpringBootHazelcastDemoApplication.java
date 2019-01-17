51442e106a12402643fd64b2d337f63295ddca9b
unknown <memojja1907@gmail.com>
2019-01-17 21:46:16 +0300

    init commit

---
+++ src/main/java/com/springboothazelcastdemo/springboothazelcastdemo/SpringBootHazelcastDemoApplication.java
@@ -0,0 +1,14 @@
+package com.springboothazelcastdemo.springboothazelcastdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHazelcastDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHazelcastDemoApplication.class, args);
+    }
+
+}
+
